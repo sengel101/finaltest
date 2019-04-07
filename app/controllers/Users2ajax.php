bccc3899130cac0b2b774a13c91d756a4bbbd535
Shloime Engel <sengel613@gmail.com>
2019-04-07 12:48:53 -0400

    These changes were made before 12:20 but not committed ?

--- app/controllers/Users2ajax.php
+++ app/controllers/Users2ajax.php
@@ -3,10 +3,11 @@ document.getElementById("login").addEventListener("click", postLogin);
 function postLogin(e){
     e.preventDefault();
     var email = document.getElementById('email').value;
-    var params = "name=" + name;
+    var pass = document.getElementById('password').value;
+    var params = "name=" + name + ",password=" + pass;
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "process.php", true);
+    xhr.open("POST", "Users2ajax.php", true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     xhr.onload = function(){
